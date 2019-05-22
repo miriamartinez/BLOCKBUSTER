bfba8743a3fc860c16de172dec9b8ee4d7b7683d
Your Name <you@example.com>
2019-05-22 08:51:54 -0500

    migrations-hecho

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -14,11 +14,12 @@ class CreateUsersTable extends Migration
     public function up()
     {
         Schema::create('users', function (Blueprint $table) {
-            $table->bigIncrements('id');
-            $table->string('name');
-            $table->string('email')->unique();
+            $table->bigIncrements('id_user');
+            $table->string('name',70);
+            $table->string('email',50)->unique();
             $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
+            $table->string('password',255);
+            $table->bigInteger('id_cliente');
             $table->rememberToken();
             $table->timestamps();
         });
